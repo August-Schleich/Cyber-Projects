663ec822e1b35da9f3ec5b390ee6df0e999b8e62
August Schleich <61630956+A4Atom@users.noreply.github.com>
2025-01-02 15:07:37 -0800

    Python IDS setup

---
+++ Python-IDS/ids.py
@@ -0,0 +1,24 @@
+#open new project repo
+import socket
+import struct 
+
+sus_patterns = ['danger.com','unauthorized_access']
+
+def sniff_packet():
+    sock =socket.socket(socket.AF_PACKET,socket.SOCK_RAW,socket.ntoh(0x0003))
+    
+    while True:
+        raw_data, addr= sock.recvfrom(65535)
+        eth_header = raw_data[:14]
+        eth_data = struct.unpack('!6s6sH')
+        
+        payload= raw_data[14:]
+        
+        for pattern in sus_patterns:
+            if pattern.encode() in payload:
+                print(f'suspicious activity detected: {pattern} in {addr}')
+
+
+if __name__ == '__main__':
+    print('Starting packet sniffer, intrusion detection')
+    sniff_packet()
